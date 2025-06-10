e4cd812916a015f7c95c534812874c003839d942
salim alhassan <salhassan111@gmail.com>
2025-06-10 16:43:13 +0000

    nothing changed

--- src/components/layout/AppSidebar.tsx
+++ src/components/layout/AppSidebar.tsx
@@ -8,9 +8,9 @@ export default function AppSidebar() {
   return (
     <aside className="hidden border-r bg-sidebar text-sidebar-foreground md:block w-64">
       <div className="flex h-full max-h-screen flex-col gap-2">
-        <div className="flex h-40 items-center justify-center border-b border-sidebar-border px-6">
+        <div className="flex h-48 items-center justify-center border-b border-sidebar-border px-6">
           <Link href="/dashboard" className="flex flex-col items-center gap-2 text-lg font-semibold text-sidebar-primary">
-            <Image src={appLogo} alt="Ledna Platform Logo" width={252} height={252} className="h-32 w-32" data-ai-hint="company logo" />
+            <Image src={appLogo} alt="Ledna Platform Logo" width={252} height={252} className="h-36 w-36" data-ai-hint="company logo" />
             <span className="font-headline">Ledna Platform</span>
           </Link>
         </div>
